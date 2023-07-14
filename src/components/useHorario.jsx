3f17c89a3bc0378631ca38bd6497196da9c959f5
allioneandrea <139451226+allioneandrea@users.noreply.github.com>
2023-07-14 20:06:10 +0200

    Update useHorario.jsx

--- src/components/useHorario.jsx
+++ src/components/useHorario.jsx
@@ -23,7 +23,7 @@ function useHorario(){
             const diffH = parseInt(fin.diff(inicio, "hours"))
             const diffM = fin.diff(inicio,'minutes')%60
 
-            if(inicioMM == mont) {
+            if(inicioMM == mont && fin) {
                 horas += diffH
                 minutes += diffM
             }
